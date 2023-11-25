8ad06b3128c219ea2af0f4d2197095d1a5bf2756
Carolina Jimenez Vargas. <carolinajimenez020@gmail.com>
2023-11-25 10:46:14 -0500

    .

--- index.js
+++ index.js
@@ -1,32 +1,89 @@
 const express = require("express");
+require("dotenv").config();
 const port = 3000;
 
 const app = express();
-const listEditRouter = require("./list-edit-router");
-const listViewRouter = require("./list-view-router");
+
 const TaskList = [
     {
         "id":"123456",
         "isCompleted":false,
         "description":"Walk the dog",
     },
-]
+];
 
-app.use("/edit", listEditRouter);
-app.use("/view", listViewRouter);
+app.use(express.json());
 app.use((req, res, next) => {
     const method = req.method;
-    const methods = ["POST", "PUT", "DELETE", "GET"]
+    const methods = ["POST", "PUT", "DELETE", "GET"];
     
-    methods.includes(method) ? next() : res.status(400).send("Método inválido, intente otro método");
-})
+    methods.includes(method) ? next() : res.status(400).send("Método inválido");
+});
 
-
+const errorValidation = (req, res, next) => {
+    const {method, body} = req;
+  
+    if (method === "POST"){
+      !Object.keys(req.body).length ? res.status(400).send("No se recibieron datos") : body.description ? next() : res.status(400).send("Los datos están incompletos");
+    };
+};
 
 app.get("/", (req, res) => {
-    res.send(JSON.stringify(TaskList));
-})
+    res.status(200).send(JSON.stringify(TaskList));
+});
+
+app.get("/completed", (req, res) => {
+    res.status(200).send(JSON.stringify(TaskList.filter((task) => task.isCompleted)));
+});
+
+app.get("/ongoing", (req, res) => {
+    res.status(200).send(JSON.stringify(TaskList.filter((task) => !task.isCompleted)));
+});
+
+app.get("/task/:id", (req, res) => {
+    const id= req.params.id;
+    const selectedTask = TaskList.find((task) => task.id == id);
+
+    selectedTask ? res.status(200).send(JSON.stringify(selectedTask)) : res.status(401).send("El ID no es válido");
+});
+
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const index = TaskList.findIndex((task) => task.id == id);
+
+  if (index == -1) {
+    res.status(401).send("El ID no es válido");
+  } else {
+    TaskList[index].isCompleted = !TaskList[index].isCompleted;
+    res.status(200).send(`Tarea con ID No. ${id} actualizada con éxito`);
+  };
+});
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const index = TaskList.findIndex((task) => task.id == id);
+
+  if (index == -1) {
+    res.status(401).send("El ID no es válido");
+  } else {
+    TaskList.splice(index, 1);
+    res.status(200).send(`Se eliminó la tarea con ID No. ${id}`);
+  };
+});
+
+app.post("/", errorValidation, (req, res) => {
+  description = req.body.description;
+  id= TaskList[TaskList.length - 1].id + 1;
+
+  TaskList.push({
+    id,
+    description,
+    isCompleted : false,
+  });
+  
+  res.status(200).send("La tarea se ha creado con éxito");
+});
 
 app.listen(port, (error) => {
     error ? console.log(error) : console.log("server listening...");
-})
+});
---
+++ review-3.md
@@ -0,0 +1,17 @@
+### ¿Qué es mi producto y para qué sirve?
+
+El producto es una API web basada en Node.js y Express que facilita la gestión de listas de tareas. Su objetivo fundamental es proporcionar a los usuarios una plataforma para realizar operaciones CRUD (Crear, Leer, Actualizar y Eliminar) en listas de tareas. Los usuarios interactúan con esta API para mantener un registro organizado de sus tareas, facilitando la gestión y seguimiento de sus actividades diarias.
+
+### ¿Cuáles son las funcionalidades más importantes y por qué los usuarios las usarían?
+
+- **Creación de Tareas Nuevas:** Permite a los usuarios agregar nuevas tareas a sus listas. Es fundamental para registrar y organizar responsabilidades pendientes.
+  
+- **Obtención de Lista de Tareas:** Proporciona una visión general de todas las tareas. Ayuda a los usuarios a tener una perspectiva completa de su carga de trabajo y les permite planificar eficientemente.
+  
+- **Filtrado por Estado de Tareas:** Facilita a los usuarios enfocarse en tareas específicas según su estado (completadas o en curso). Mejora la organización y el seguimiento del progreso.
+  
+- **Detalle de Tarea por ID:** Ofrece información específica sobre una tarea determinada. Permite comprender detalles y estado para un seguimiento más detallado.
+  
+- **Actualización del Estado de Tarea:** Permite marcar tareas como completadas o en progreso. Ayuda a mantener al día el estado de las tareas y a medir el progreso.
+  
+- **Eliminación de Tareas:** Facilita la eliminación de tareas no relevantes o ya completadas. Contribuye a mantener la lista de tareas ordenada y actualizada.
